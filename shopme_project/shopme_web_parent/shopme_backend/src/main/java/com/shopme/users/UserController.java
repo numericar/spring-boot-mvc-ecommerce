bdedddc7c332f6c2cdba3bff75b34cb1374df580
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2024-12-19 21:14:56 +0700

    function create user

--- shopme_project/shopme_web_parent/shopme_backend/src/main/java/com/shopme/users/UserController.java
+++ shopme_project/shopme_web_parent/shopme_backend/src/main/java/com/shopme/users/UserController.java
@@ -3,8 +3,11 @@ package com.shopme.users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.commons.entities.Role;
 import com.shopme.commons.entities.User;
 
 @Controller
@@ -18,7 +21,7 @@ public class UserController {
     }
 
     @GetMapping()
-    public String viewIndex(Model model) {
+    public String viewIndexPage(Model model) {
         Iterable<User> users = this.userService.listAll();
 
         model.addAttribute("users", users);
@@ -26,4 +29,27 @@ public class UserController {
         return "users/users_index";
     }
 
+    @GetMapping("/create")
+    public String viewCreatePage(Model model) {
+        User user = new User();
+        Iterable<Role> roles = this.userService.listRoles();
+
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+
+        return "users/users_create";
+    }
+
+
+    // RedirectAttribute ใช้สำหรับเพิ่มข้อมูลในการ redirect ไปที่หน้าอื่น
+    @PostMapping("/create")
+    public String createUserHandler(User user, RedirectAttributes redirectAttributes) {
+        System.out.println(user.toString());
+
+        this.userService.save(user);
+
+        redirectAttributes.addFlashAttribute("message", "The user has been created succesfully");
+
+        return "redirect:/users";
+    }
 }
--- shopme_project/shopme_web_parent/shopme_backend/src/main/java/com/shopme/users/UserService.java
+++ shopme_project/shopme_web_parent/shopme_backend/src/main/java/com/shopme/users/UserService.java
@@ -2,18 +2,29 @@ package com.shopme.users;
 
 import org.springframework.stereotype.Service;
 
+import com.shopme.commons.entities.Role;
 import com.shopme.commons.entities.User;
 
 @Service
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     public Iterable<User> listAll() {
         return this.userRepository.findAll();
+    }
+
+    public Iterable<Role> listRoles() {
+        return this.roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        this.userRepository.save(user);
     }
 }
